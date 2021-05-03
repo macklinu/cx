1311136a0ad18ba563cf0d9e3e98c587dd3884e5
Mackie Underdown <macklinu@users.noreply.github.com>
2021-05-03 16:01:50 -0400

    Update dependencies

--- src/index.test.js
+++ src/index.test.js
@@ -1,15 +1,15 @@
 import test from 'ava'
 import cx from '.'
 
-test('returns empty string when passed no args', t => {
+test('returns empty string when passed no args', (t) => {
   t.is(cx(), '')
 })
 
-test('returns empty string when passed falsy arguments', t => {
+test('returns empty string when passed falsy arguments', (t) => {
   t.is(cx(null, undefined, 0, false, void 0), '')
 })
 
-test('concatenates truthy string arguments into classNames string', t => {
+test('concatenates truthy string arguments into classNames string', (t) => {
   t.is(
     cx('kareem', 'abdul', 'jabbar', void 0 && 'larry-bird', 123),
     'kareem abdul jabbar'
