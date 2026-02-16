09c888ca7619e672ebf98876a442d4ad48c2f1a2
Mackie Underdown <macklinu@users.noreply.github.com>
2026-02-16 10:41:27 -0500

    Update to v3

---
+++ .changeset/frank-mammals-worry.md
@@ -0,0 +1,5 @@
+---
+"@macklinu/cx": major
+---
+
+Upgrade tailwind-merge@^3 and Node 24
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export const cx = (...inputs: ClassValue[]) => twMerge(clsx(...inputs))
+/**
+ * A combination of "clsx" and "tailwind-merge" libraries.
+ *
+ * @see https://github.com/lukeed/clsx
+ * @see https://github.com/dcastil/tailwind-merge
+ */
+export const cx = (...inputs: ClassValue[]) => twMerge(clsx(...inputs));
